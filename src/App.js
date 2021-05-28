fc870197cb8b5839bb5b9bf4d1bfd5dfe13ba4b0
mikolaj.sas <beqs171@gmail.com>
2021-05-28 14:54:37 +0200

    'lab12'

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import { Route, Switch, Redirect } from "react-router-dom";
 import Posts from "./components/posts";
 import NotFound from "./components/notFound";
+import SingUpForm from "./components/signUp";
 import Home from "./components/home";
 import LoginForm from "./components/loginForm";
 import NavBar from "./components/common/navbar";
-
+import {isExpired} from "react-jwt";
 
 
 function App() {
@@ -17,7 +18,15 @@ function App() {
             <div className="content">
                 <Switch>
                     <Route path="/login" component={LoginForm} />
-                    <Route path="/posts" component={Posts} />
+                    <Route path="/signUp" component={SingUpForm} />
+                    <Route path="/posts"
+                           render={props => {
+                               if (isExpired(localStorage.getItem('token'))) {
+                                   return <Redirect to="/" />;
+                               }
+                               return <Posts />;
+                           }}
+                    />
                     <Route path="/not-found" component={NotFound} />
                     <Route path="/" exact component={Home} />
                     <Redirect to="/not-found" />
--- src/components/signUp.jsx
+++ src/components/signUp.jsx
@@ -1,15 +1,22 @@
 import React, {Component} from "react";
 const axios = require('axios');
 
-class LoginForm extends Component {
+class SingUpForm extends Component {
 
     state = {
         account: {
             username: "",
+            email: "",
             password: ""
         },
-        errors: {}
+        errors:{}
+
     };
+
+    handleChangeRoute = () =>{
+        this.props.history.push('/');
+        window.location.reload();
+    }
 
     validate = () => {
         const errors = {};
@@ -18,17 +25,15 @@ class LoginForm extends Component {
         if (account.username.trim() === '') {
             errors.username = 'Username is required!';
         }
+        if (account.email.trim() === '') {
+            errors.email = 'Email is required!';
+        }
         if (account.password.trim() === '') {
             errors.password = 'Password is required!';
         }
 
         return Object.keys(errors).length === 0 ? null : errors;
     };
-
-    handleChangeRoute = () =>{
-        this.props.history.push('/');
-        window.location.reload();
-    }
 
     handleSubmit = (event) => {
         event.preventDefault();
@@ -37,15 +42,17 @@ class LoginForm extends Component {
         this.setState({errors: errors || {}});
         if (errors) return;
 
+        console.log(this.state)
+
         axios({
             method: 'post',
-            url: 'http://localhost:3001/api/user/auth',
+            url: 'http://localhost:3001/api/user/create',
             data: {
-                login: this.state.account.username,
+                name: this.state.account.username,
+                email: this.state.account.email,
                 password: this.state.account.password
             }
         }).then((response) => {
-            localStorage.setItem('token', response.data.token);
             this.handleChangeRoute();
         }).catch((error) => {
             const errors = {};
@@ -64,10 +71,10 @@ class LoginForm extends Component {
     render() {
         return (
             <div>
-                <h1>Login</h1>
+                <h1>Sign Up</h1>
                 <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
-                        <label htmlFor="username">Email address</label>
+                        <label htmlFor="username">Name</label>
                         <input value={this.state.account.username}
                                name="username"
                                onChange={this.handleChange}
@@ -78,6 +85,19 @@ class LoginForm extends Component {
                                placeholder="Username"/>
                         {this.state.errors.username &&
                         <div className="alert alert-danger">{this.state.errors.username}</div>}
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email address</label>
+                        <input value={this.state.account.email}
+                               name="email"
+                               onChange={this.handleChange}
+                               type="email"
+                               className="form-control"
+                               id="email"
+                               aria-describedby="emailHelp"
+                               placeholder="Email"/>
+                        {this.state.errors.email &&
+                        <div className="alert alert-danger">{this.state.errors.email}</div>}
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Password</label>
@@ -91,7 +111,7 @@ class LoginForm extends Component {
                         {this.state.errors.password &&
                         <div className="alert alert-danger">{this.state.errors.password}</div>}
                     </div>
-                    <button type="submit" className="btn btn-primary">Login</button>
+                    <button type="submit" className="btn btn-primary">SignUp</button>
                 </form>
 
             </div>
@@ -99,4 +119,8 @@ class LoginForm extends Component {
     }
 }
 
-export default LoginForm;
+export default SingUpForm;
+
+
+
+
